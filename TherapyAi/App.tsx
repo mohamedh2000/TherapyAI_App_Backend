5364276587512b70e105fb76e75a1b3019ce6abc
mohamed2000 <husseinmohamed202200@gmail.com>
2025-06-05 15:05:53 -0400

    Adding front-end

---
+++ TherapyAi/App.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  ListRenderItem,
+} from 'react-native';
+import { useEffect } from 'react';
+import {
+  AudioSession,
+  LiveKitRoom,
+  useTracks,
+  TrackReferenceOrPlaceholder,
+  VideoTrack,
+  isTrackReference,
+  registerGlobals,
+} from '@livekit/react-native';
+import { Track } from 'livekit-client';
+
+registerGlobals();
+
+// !! Note !!
+// This sample hardcodes a token which expires in 2 hours.
+const wsURL = "wss://stt-llm-tts-3cnt5csx.livekit.cloud"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDkxMzY1NzYsImlzcyI6IkFQSVdqajlqQkx2OW85cSIsIm5iZiI6MTc0OTEyOTM3Niwic3ViIjoicXVpY2tzdGFydCB1c2VyIHA1Z2FlZyIsInZpZGVvIjp7ImNhblB1Ymxpc2giOnRydWUsImNhblB1Ymxpc2hEYXRhIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWUsInJvb20iOiJxdWlja3N0YXJ0IHJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.vsOkx2uAM_nt3qh8HDoDhml1e5ZA-Zeqvtq0j3Bf9TE"
+
+export default function App() {
+  // Start the audio session first.
+  useEffect(() => {
+    let start = async () => {
+      await AudioSession.startAudioSession();
+    };
+
+    start();
+    return () => {
+      AudioSession.stopAudioSession();
+    };
+  }, []);
+
+  return (
+    <LiveKitRoom
+      serverUrl={wsURL}
+      token={token}
+      connect={true}
+      options={{
+        // Use screen pixel density to handle screens with differing densities.
+        adaptiveStream: { pixelDensity: 'screen' },
+      }}
+      audio={true}
+      video={true}
+    >
+      <RoomView />
+    </LiveKitRoom>
+  );
+};
+
+const RoomView = () => {
+  // Get all camera tracks.
+  const tracks = useTracks([Track.Source.Camera]);
+
+  const renderTrack: ListRenderItem<TrackReferenceOrPlaceholder> = ({item}) => {
+    // Render using the VideoTrack component.
+    if(isTrackReference(item)) {
+      return (<VideoTrack trackRef={item} style={styles.participantView} />)
+    } else {
+      return (<View style={styles.participantView} />)
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tracks}
+        renderItem={renderTrack}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  participantView: {
+    height: 300,
+  },
+});
